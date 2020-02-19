5bb98d45de4128db923b6160737ee039d648ce9f
Hussain Aminu <hussain_aminu@yahoo.com>
2020-02-20 00:06:28 +0300

    folder restructuring for seperation of concern

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -67,7 +67,7 @@ class App extends Component {
             totalProducts: parseInt(res.headers.get("x-total-count"))
 
           });
-          console.log(totalProducts);
+
         }
 
         return res.json();
