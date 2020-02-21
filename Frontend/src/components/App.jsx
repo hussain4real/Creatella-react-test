5a03ab8a786e7c3098728c0ab2f946ec0bdd59e7
Hussain Aminu <hussain_aminu@yahoo.com>
2020-02-21 17:15:25 +0300

    little styling

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -178,13 +178,16 @@ class App extends Component {
 
     return (
       <div className="container">
+        <div className="text-center">
+
         <h1>Products Grid</h1>
-        <p>
+        <p className="font-weight-bold">
           Here you are sure to find a bargain on some of the finest ascii
           available to purchase. Be sure to peruse our selection of ascii faces
           in an exciting range of sizes and prices.
-        </p>
-        <p>But first, a word from our sponsors:</p>
+        <br/>
+        But first, a word from our sponsors:</p>
+        </div>
 
         <div className="text-center">
           <Ad />
